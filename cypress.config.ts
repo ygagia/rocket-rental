7892226c81dc8d69b31ccf55bc3ab2d0db092e06
John <j.gagiatsos@gmail.com>
2022-11-05 01:55:46 +0200

    Setup cypress project key

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "ekq3q6",
   e2e: {
     setupNodeEvents: (on, config) => {
       const isDev = config.watchForFileChanges;
